01c10a85d320e0ee6089436fbedf15e682da037c
ymclapp <ymclapp@yahoo.com>
2021-01-22 18:03:40 -0600

    YES!!  Images are loading and got dropdown to populate

--- js/app.js
+++ js/app.js
@@ -6,12 +6,67 @@ $(function() {
   console.log('Document is ready!');
 });
 
+$('#bt').click(function () {
+  var file = "data/page-1.json";
+
+  $.getJSON(file,function (data) {
+    $.each(data, function (index, value) {
+      //Append or insert data to select element
+      $('#sel').append('<option value = " ' + value.ID + '">' + value.keyword + '</option>');
+    });
+  });
+});
+
+//Show selected value
+$('#sel').change(function () {
+  $('#msg').text('Selected Item:  ' + this.options[this.selectedIndex].text);
+});
+
+function Horn(horn) {
+  this.title = horn.title;
+  this.image_url = horn.image_url;
+  this.description = horn.description;
+  this.keyword = horn.keyword;
+  this.horns = horn.horns;
+}
+
+Horn.prototype.render = function(container) {
+  let $container = $(container);
+  let $template = $container.find('.horn-template');
+  let $horn = $template.clone();
+  $horn.removeClass('horn-template');
+  $horn.find('.horn-title').text(this.title);
+  $horn.find('img.horn-image').attr('src',this.image_url);
+  $horn.find('.horn-description').text('Description:  ' + this.description);
+  $container.append($horn);
+}
+
+// var select = document.getElementsByClassName('horn-keyword');
+// var list = this.keyword;
+// $('.horn-keyword').empty();
+// $.each(list, function (i, p) {
+//   $('.horn-keyword').append($('<option></option>').val(p).html(p))
+// }); 
+
+
+$('main section').hide();
 const ajaxSettings = {
-  method: 'get',
+  method:  'get',
   dataType:  'json'
 };
+
+
+
+
 console.log('about to AJAX', ajaxSettings);
-$.ajax('/data/page-1.json', ajaxSettings)
+$.ajax('data/page-1.json', ajaxSettings)
   .then(function (data) {
     console.log(data);
+    const horn = data;
+    horn.forEach(horn => {
+      console.log(horn.title);
+      let actualHorn = new Horn(horn);
+      console.log(actualHorn);
+      actualHorn.render('main section');
+    })
   });
